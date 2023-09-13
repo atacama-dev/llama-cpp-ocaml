448d37881e8e6c8a553a3dbe3cc265ba86ffb640
Ilias Garnier <igarnier@protonmail.com>
2023-09-13 22:46:09 +0200

    Fix macos build

--- src/config/discover.ml
+++ src/config/discover.ml
@@ -5,19 +5,18 @@ let rec link ?(flag = "-l") = function
   | lib :: libs -> "-cclib" :: (flag ^ " " ^ lib) :: link ~flag libs
 
 let () =
-  C.main ~name:"raylib" (fun c ->
+  C.main ~name:"llama.cpp" (fun c ->
       let library_flags =
         match C.ocaml_config_var c "system" with
         | Some ("linux" | "linux_elf" | "elf") ->
           link [ "llama_bindings"; "stdc++" ]
         | Some "macosx" ->
-          (* TODO *)
-          link [ "llama_bindings"; "stdc++" ]
+          link ~flag:"-framework" [ "Accelerate"; "Foundation"; "Metal"; "MetalKit" ]
+          @ link [ "llama_bindings"; "stdc++" ]
         | Some "mingw64" ->
           (* TODO *)
           link [ "llama_bindings"; "stdc++" ]
         | Some ("netbsd" | "freebsd" | "openbsd" | "bsd" | "bsd_elf") ->
-          (* TODO *)
           link [ "llama_bindings"; "stdc++"]
         | Some system -> C.die "unsupported system: %s" system
         | None -> C.die "unsupported system"
